3eeb40d1fb17f5735f5bb55a55a5bbfa1f8f3241
JangoBoogaloo <jango.boogaloo@gmail.com>
2015-11-02 21:49:57 -0500

    set vimrc to user profile

--- linux/link_vimrc.sh
+++ linux/link_vimrc.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo Back up current vimrc
-sudo mv /etc/vim/vimrc /etc/vim/vimrc.bak
 echo Put custom vimrc
-sudo cp setting/vimrc /etc/vim/vimrc
+sudo cp setting/vimrc ~/.vimrc
--- mac/link_vimrc.sh
+++ mac/link_vimrc.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-echo Back up current vimrc
-sudo mv /usr/share/vim/vimrc /usr/share/vim/vimrc.bak
 echo Put custom vimrc
-sudo cp ../linux/setting/vimrc /usr/share/vim/
+sudo cp ../linux/setting/vimrc ~/.vimrc
